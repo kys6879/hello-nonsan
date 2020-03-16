daa45682ed2c7e8f7f6b21b4aaabf9675571aeca
kys6879 <kys6879@naver.com>
2020-03-17 00:49:18 +0900

    add counting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { CounterComponent } from './counter/counter.component';
+import { read } from 'fs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'hello-nonsan';
+
+  @ViewChild('counter', {
+    read: CounterComponent,
+    static: true
+  })
+  private counter: CounterComponent;
+
+  ngOnInit(): void {
+    this.counter.start();
+    this.counter.counterState.subscribe(state => {
+      if (state === 'COMPLETE') {
+        alert('Done!');
+        this.counter.stop();
+      }
+    });
+  }
+
+
 }
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit, Input, Output, EventEmitter, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
+import { Observable, interval, Subscription } from 'rxjs';
+
+import { take } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
+import { runInThisContext } from 'vm';
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class CounterComponent implements OnInit {
+
+  startAt = 5;
+
+  @Output()
+  counterState = new EventEmitter<string>();
+
+  currentValue = '';
+
+  currentSubscription: Subscription;
+
+
+  constructor(private changeDetector: ChangeDetectorRef) { }
+
+  ngOnInit() {
+
+  }
+
+  public start() {
+    this.currentValue = this.formatValue(this.startAt);
+    this.changeDetector.detectChanges();
+
+    const t: Observable<number> = interval(1000);
+
+    this.currentSubscription = t.pipe(take(this.startAt)).map(v => this.startAt - (v + 1)).subscribe(v => {
+      this.currentValue = this.formatValue(v);
+      this.changeDetector.detectChanges();
+    }, err => {
+      this.counterState.error(err);
+    }, () => {
+      this.currentValue = '00:00';
+      this.counterState.emit('COMPLETE');
+      this.changeDetector.detectChanges();
+    });
+  }
+
+  public stop() {
+    this.currentSubscription.unsubscribe();
+    this.counterState.emit('ABORTED');
+  }
+
+  private formatValue(v) {
+    const minutes = Math.floor(v / 60);
+
+    const formattedMinutes = (minutes > 9 ? minutes : '0' + minutes);
+
+    const seconds = v % 60;
+
+    const formattedSeconds = (seconds > 9 ? seconds : '0' + seconds);
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+
+
+  }
+
+}
