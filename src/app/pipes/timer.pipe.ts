7ef703035a29bc6555e5c8e438ed31d4006bad61
kys6879 <kys6879@naver.com>
2020-03-20 18:50:46 +0900

    change format

--- src/app/pipes/timer.pipe.ts
+++ src/app/pipes/timer.pipe.ts
@@ -7,7 +7,7 @@ import { Timer } from '../models/timer.model';
 export class TimerPipe implements PipeTransform {
 
   transform(value: Timer, ...args: any[]) {
-    return value.days;
+    return `${value.days}일 ${value.hours}시간 ${value.minutes}분 ${value.seconds}초`;
   }
 
 
